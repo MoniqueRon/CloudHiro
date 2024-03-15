9566f015425ba2d9251b15346060fd4bb70520dd
Monique Ron <105851374+MoniqueRon@users.noreply.github.com>
2024-03-15 17:42:55 +0200

    Add files via upload
    Finalized and tested with postman - GET all data limited to 100 entries

--- assignment.py
+++ assignment.py
@@ -47,38 +47,41 @@ def execute_query(query, fetch=True):
         cur.close()
         conn.close()
 
+def init_db():
+    global table_name
+    try:
+        # Download the zip file from the URL with password and extract the csv
+        response = requests.get(url)
+        if response.status_code == 200:
+            with zipfile.ZipFile(BytesIO(response.content)) as zip_file:
+                zip_file.extractall(path=".", pwd=url_password.encode('utf-8'))
 
-try:
-    # Download the zip file from the URL with password and extract the csv
-    response = requests.get(url)
-    if response.status_code == 200:
-        with zipfile.ZipFile(BytesIO(response.content)) as zip_file:
-            zip_file.extractall(path=".", pwd=url_password.encode('utf-8'))
+                # upload the extracted file to the PostgreSQL database
+                for file in zip_file.namelist():
+                    with open(file, 'r') as f:
+                        table_name = os.path.basename(file).replace('-', '_').replace('.csv', '')
+                        if table_name[0].isdigit():
+                            table_name = '_' + table_name
 
-            # upload the extracted file to the PostgreSQL database
-            for file in zip_file.namelist():
-                with open(file, 'r') as f:
-                    table_name = os.path.basename(file).replace('-', '_').replace('.csv', '')
-                    if table_name[0].isdigit():
-                        table_name = '_' + table_name
+                        conn = psycopg2.connect(host=hostname,
+                                                database=database,
+                                                user=username,
+                                                password=password,
+                                                port=port_id)
+        
+                        cur = conn.cursor()
+                        cur.copy_expert(f"COPY {table_name} FROM STDIN WITH CSV HEADER", f)
+                        conn.commit()
+                        print(f"{table_name} uploaded to PostgreSQL successfully")
+    except Exception as error:
+        print(error)
+    finally:
+        cur.close()
+        conn.close()
 
-                    conn = psycopg2.connect(host=hostname,
-                                            database=database,
-                                            user=username,
-                                            password=password,
-                                            port=port_id)
-    
-                    cur = conn.cursor()
-                    cur.copy_expert(f"COPY {table_name} FROM STDIN WITH CSV HEADER", f)
-                    conn.commit()
-                    print(f"{table_name} uploaded to PostgreSQL successfully")
-except Exception as error:
-    print(error)
-finally:
-    cur.close()
-    conn.close()
-
-    
+@app.before_request
+def startup():
+    init_db()
 # Define routes for CRUD operations:
 
 # Create new data
@@ -95,8 +98,9 @@ def create_data():
 # Read data
 @app.route('/data', methods=['GET'])
 def read_all_data():
-    query = f"SELECT * FROM {table_name}"
+    query = f"SELECT * FROM {table_name} LIMIT 100"
     data = execute_query(query)
+    print('oi', data)
     return jsonify(data)
 
 @app.route('/data/<int:id>', methods=['GET'])
@@ -132,7 +136,7 @@ def delete_data(id):
         return jsonify({'message': f'Error deleting data with id {id}'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
 
 
